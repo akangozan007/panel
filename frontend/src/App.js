d05bacefdde4ae88a1e8d62f871db523a1997fab
Muhammad Razan Rizqullah <razan@dosqla.local>
2025-05-31 12:45:13 +0700

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+
+import Login from './pages/Login';
+function App() {
+  return (
+    <>
+    <div className='container-fluid'>
+     <Login />
+    </div>
+    </>
+  );
+}
+
+export default App;
