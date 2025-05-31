2d1090e441a49ca7e041b6ce1e7231ca28e24f56
Muhammad Razan Rizqullah <razan@dosqla.local>
2025-05-31 17:28:04 +0700

    update dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-
 import Login from './pages/Login';
+import Dashboard from './dashboard/Dashboard';
+
 function App() {
   return (
     <>
-    <div className='container-fluid'>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+    {/* <div className='container-fluid'>
      <Login />
-    </div>
+    </div> */}
     </>
   );
 }
